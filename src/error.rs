5cb158dbcfee46799078426d3f4903a8014e53f6
Tobias Happ <tobias.happ@gmx.de>
2019-01-15 20:20:17 +0100

    Implement WrapErrorExt for io errors and optionals

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use std::io;
 use wrapper::config;
 
 pub use std::result::Result as StdResult;
@@ -6,7 +7,9 @@ pub type Result<T> = StdResult<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     HookFailed,
-    InvalidConfig(String, config::ConfigError),
+    General(String),
+    InvalidConfig(String, Box<config::ConfigError>),
+    IoConfig(String, io::Error),
 }
 
 pub(crate) trait WrapErrorExt<T> {
@@ -20,6 +23,24 @@ impl<T> WrapErrorExt<T> for StdResult<T, config::ConfigError> {
     where
         M: Into<String>,
     {
-        self.map_err(|error| Error::InvalidConfig(message.into(), error))
+        self.map_err(|error| Error::InvalidConfig(message.into(), error.into()))
+    }
+}
+
+impl<T> WrapErrorExt<T> for StdResult<T, io::Error> {
+    fn wrap_error<M>(self, message: M) -> Result<T>
+    where
+        M: Into<String>,
+    {
+        self.map_err(|error| Error::IoConfig(message.into(), error))
+    }
+}
+
+impl<T> WrapErrorExt<T> for Option<T> {
+    fn wrap_error<M>(self, message: M) -> Result<T>
+    where
+        M: Into<String>,
+    {
+        self.ok_or_else(|| Error::General(message.into()))
     }
 }
